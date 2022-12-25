a5d33f84d67287252bef9e572d79bc1b56738f2f
Vitaliy Sklyar <vitaliy.sklyar@binarapps.com>
2022-12-25 16:08:05 +0100

    add models, controlers, views

---
+++ WebApp_SQL_Server/Data/ViewModels/PersonVM.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp_Sql_Server.Data.ViewModels
+{
+    public class PersonVM
+    {
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+    }
+}
